cae4dd32c64378c7ac47e60b08ea8198618c0eba
Oleg G.Kapranov <lugatex@yahoo.com>
2017-05-31 12:44:38 +0300

    added
        Bootstrap 4 components for Vue.js 2

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './app.vue'
+import BootstrapVue from 'bootstrap-vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   document.body.appendChild(document.createElement('hello'))

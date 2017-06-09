b779208efa6d986f41e46990d9fc67afcf661e32
Oleg G.Kapranov <lugatex@yahoo.com>
2017-06-09 11:56:26 +0300

    created Books controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
   root to: "tops#index"
 
   get 'demo/index'
   get 'home/index'
   get 'employees/index'
   get 'sortlists/index'
+
+  resources :books
 
   mount ActionCable.server => '/cable'
 end
